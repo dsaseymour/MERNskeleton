df76d2d4e041c4c0cbaa6fb3ae3c88e6fc26cd81
Danny <dannyseymour1990@gmail.com>
2019-02-03 18:22:03 -0500

    resolved issue caused by differences between user model and user object created in registerUser contoller method

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -9,7 +9,7 @@ const isEmpty = require("is-empty");
 
 //
 signToken = userJWTPayload => {
-  return JWT.sign(
+  return jwt.sign(
     userJWTPayload,
     process.env.JWT_SECRET,
     { expiresIn: 3600 },
@@ -55,24 +55,27 @@ module.exports = {
     }
     //user email does not already exist, create user
     const newUser = new User({
-      name: req.body.name,
-      email: req.body.email,
-      password: req.body.password
+      method: "local",
+      local: {
+        email: req.body.email,
+        passwordHash: req.body.password
+      },
+      displayName: req.body.name
     });
     //generate password hash
-
     bcrypt.genSalt(10, (err, salt) => {
-      bcrypt.hash(newUser.password, salt, (err, hash) => {
-        //        if (err) throw err;
-        newUser.password = hash;
-        newUser.save();
-        return res.status(200).json({
-          status: "Success",
-          operation: "User Registration",
-          newUser
-        });
+      bcrypt.hash(req.body.password, salt, (err, hash) => {
+        if (err) {
+          throw err;
+        }
+        newUser.local.passwordHash = hash;
+        newUser
+          .save()
+          .then(user => res.json(user))
+          .catch(err => console.log(err));
       });
     });
+
     //user has been created
   },
   localLogin: async (req, res, next) => {
@@ -95,7 +98,7 @@ module.exports = {
 
     const passwordMatch = await bcrypt.compare(
       plaintextpassword,
-      foundUser.password
+      foundUser.local.passwordHash
     );
 
     if (passwordMatch) {
