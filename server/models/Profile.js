c0d12978dab077098590561213c918f2974970c0
Danny <dannyseymour1990@gmail.com>
2019-02-05 16:42:14 -0500

    updating profile schema with new fields

--- server/models/Profile.js
+++ server/models/Profile.js
@@ -5,7 +5,40 @@ const ProfileSchema = new Schema({
     type: Schema.Types.ObjectId,
     ref: "users"
   },
-  handle: { type: String, required: [true, "Handle is required"], max: 40 },
+  handle: {
+    type: String,
+    required: [true, "Handle is required"],
+    max: 40
+  },
+  photo: {
+    type: String
+  },
+  age: {
+    type: Number
+  },
+  dateofbirth: {
+    type: Date
+  },
+  address: {
+    type: String
+  },
+  gender: {
+    type: String
+  },
+  job: {
+    type: String
+  },
+  hobbies: [
+    {
+      description: String
+    }
+  ],
+  website: {
+    type: String
+  },
+  bio: {
+    type: String
+  },
   social: {
     youtube: { type: String },
     twitter: { type: String },
